41734a0ec66330d55a047759c360f728ffa5ccdf
MOAAS <38762849+MOAAS@users.noreply.github.com>
2022-03-18 02:41:09 +0000

    Extension experimentation with Wikimedia API

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,47 +1,41 @@
-/*global chrome*/
 
-import logo from './logo.svg';
 import './App.css';
-
-
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-
-    document.body.style.backgroundColor = color;
-  });
-}
+import { useState } from 'react';
+import { getPageContent } from './WikiAPI';
+import { extensionUpdatePageBackgroundColor } from './ExtensionTest';
 
 function App() {
+  const [title, setTitle] = useState("")
+
+  const [text, setText] = useState("")
+
+  function doWikipediaStuff() {    
+    getPageContent(title).then(res => {
+
+      setText("Quality: Very good! " + res.parse.wikitext)
+      console.log(res);
+    })
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
      {/*  <div style="width: 300px;height: 300px;text-align: center; font-size: 2.5em;display: flex;flex-direction:column;align-items: center;justify-content: center;">
         This is like the best Wikipedia page ever
       
         <strong style="font-weight: bold; font-size: 2em;">A+</strong>
       </div> */}
-    
-      <button id="changeColor" onClick={async () => { 
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: setPageBackgroundColor,
-        });
-      }}></button>
+
+      <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", gap: "8px"}}>
+        <input name="title" value={title} onChange={e => setTitle(e.target.value)}/>    
+
+        <button onClick={doWikipediaStuff}>Do Wiki</button>
+
+        {/* <button onClick={extensionUpdatePageBackgroundColor}>Change Color (test)</button> */}
+      </div>
+
+      <p>{text}</p>
+
     </div>
   );
 }
---
+++ extension/src/ExtensionTest.js
@@ -0,0 +1,14 @@
+/*global chrome*/
+
+export async function extensionUpdatePageBackgroundColor() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+          chrome.storage.sync.get("color", ({ color }) => {
+            document.body.style.backgroundColor = color;
+          });
+        },
+      });  
+  }
+  
