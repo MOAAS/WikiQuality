2385d1d853b33571a4d145d5b31eecfd956bd615
MOAAS <38762849+MOAAS@users.noreply.github.com>
2022-04-14 18:39:33 +0100

    Updated titles

--- ml/src/collect_titles.py
+++ ml/src/collect_titles.py
@@ -33,7 +33,7 @@ def getWikiQualityPages(category, filename, isPageList = False):
     print("Removing duplicates and cleaning titles")
     with open(filename, 'r', encoding='utf-8') as f:
         titles = f.read().split('\n')        
-        titles = [title for title in titles if title != ''] # remove empty lines (usually only last one is empty, but better safe than sorry)
+        titles = [title for title in titles if title != '' and not title.endswith('GA1')] # remove empty lines (usually only last one is empty, but better safe than sorry)       
         titles = list(set(titles)) # remove duplicates        
         titles = [title.replace('Talk:', '') for title in titles] # remove "Talk:"
         with open(filename, 'w', encoding='utf-8') as f:
