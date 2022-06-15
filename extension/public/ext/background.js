560f104e841e8c7de11ab846c1bd52e7d08de443
MOAAS <38762849+MOAAS@users.noreply.github.com>
2022-06-15 03:59:37 +0100

    manifest update. removing unnecessary files

--- extension/public/ext/background.js
+++ extension/public/ext/background.js
@@ -1,10 +1,4 @@
 // background.js
-
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-
-
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log("Background running.")
 });
