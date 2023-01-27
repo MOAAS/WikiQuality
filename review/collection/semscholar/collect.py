707e8eac2526264dfea9701251e380abdf0b49e5
MOAAS <miguel.moas@gmail.com>
2023-01-27 18:09:30 +0000

    produced dataset for abstract scanning

--- review/collection/semscholar/collect.py
+++ review/collection/semscholar/collect.py
@@ -1,3 +1,4 @@
+import re
 import json
 from api import search_multiple
 
@@ -29,18 +30,84 @@ def assign_duplicates(articles):
         else:
             visited.add(article['semscholarId'])
 
+def extract_info(paper):      
+    def extract_pub_type():
+        if paper['publicationVenue'] is not None and 'type' in paper['publicationVenue']:
+            return paper['publicationVenue']['type']
+
+        if paper['publicationTypes'] is not None and 'Conference' in paper['publicationTypes']:
+            return 'conference'
+
+        if paper['journal'] is not None:
+            name = paper['journal'].get('name', '').lower()
+            if 'conference' in name or 'proceedings' in name or 'workshop' in name or 'symposium' in name:
+                return 'conference'
+            
+            # if it has a volume, it is a journal
+            if 'volume' in paper['journal']:
+                return 'journal'
+        
+        if paper['externalIds'] is not None and 'DBLP' in paper['externalIds']:
+            dblp = paper['externalIds']['DBLP']
+            if 'conf' in dblp:
+                return 'conference'
+            if 'journal' in dblp or 'series' in dblp:
+                return 'journal'
+            if 'book' in dblp:
+                return 'book'
+            if 'phd' in dblp or 'masters' in dblp or 'thesis' in dblp:
+                return 'thesis'
+            
+        return 'unknown'
+            
+    def extract_pub():
+        # get name. first try journal['name'], then publicationVenue['name']
+        if paper['journal'] is not None and 'name' in paper['journal'] and paper['journal']['name'] is not None and paper['journal']['name'] != '':
+            str = paper['journal']['name']
+        elif paper['publicationVenue'] is not None:
+            str = paper['publicationVenue']['name']
+        else:
+            return "N/A"
+        
+        # add volume and issue        
+        if paper['journal'] is not None:
+            journal = paper['journal']
+            if 'volume' in journal:
+                str += ', ' + journal['volume']
+                if 'issue' in journal:
+                    str += '(' + journal['issue'] + ')'
+            if 'pages' in journal:
+                str += ', pp. ' + journal['pages']
+        return str
+        
+    if paper['title'].isupper(): # if title is in all caps, capitalize it
+        paper['title'] = paper['title'].capitalize()
+
+    # replace repeated spaces with single space + repeated newlines with single newline
+    if paper['abstract'] is not None:
+        paper['abstract'] = re.sub(r'\s+', ' ', paper['abstract'])
+    
+    return {
+        'title': paper['title'],
+        'abstract': paper['abstract'],
+        'authors': '; '.join([a['name'] for a in paper['authors']]),
+        'year': paper['year'],
+        'publication_type': extract_pub_type().capitalize(),
+        'published_in': extract_pub(),
+        'num_references': paper['referenceCount'],
+        'num_citations': paper['citationCount'],
+        'url': paper['url'],
+        'doi': paper['externalIds']['DOI'] if 'DOI' in paper['externalIds'] else None,
+        'pdf': paper['openAccessPdf']['url'] if paper['openAccessPdf'] is not None else None,
+        #'paper': paper
+    }
+
 def fill_article_info(articles):
     ids = [a['semscholarId'] for a in articles if 'semscholarId' in a and a['semscholarId'] != 'not_found']
     papers = search_multiple(ids)
     papers = {p['paperId']: p for p in papers} # make it a dict for easier access
     # with open('semscholar/papers.json', 'w', encoding='utf-8') as outfile:
     #     json.dump(papers, outfile, indent=4)
-
-    def extract_info(paper):
-        return {
-            'title': paper['title'],
-            'abstract': paper['abstract'],
-        }
 
     for article in articles:
         if 'semscholarId' in article and article['semscholarId'] != 'not_found':
@@ -59,12 +126,14 @@ def fill_article_databases(articles):
             article['databases'] = [article['original']['database']]
             continue
 
-    for article in articles:
-        if 'databases' in article:
+    # remove duplicate databases + sort them
+    for article in articles: 
+        if 'databases' in article: 
             article['databases'] = list(set(article['databases']))
     
 
 def reorder_keys(articles):
+    # rename and order keys to match our needs
     for article in articles:
         if 'duplicate_of' in article or article['semscholarId'] == 'not_found':
             article['original'] = article.pop('original') # move original to end 
@@ -88,14 +157,28 @@ reorder_keys(articles)
 with open('semscholar/output.json', 'w', encoding='utf-8') as outfile:
     json.dump(articles, outfile, indent=4)
 
+print("Dumped " + str(len(articles)) + " articles to semscholar/output.json")
+
+
+# Just to help me copy to Google Sheets
 import csv
-keys = ['id', 'title', 'databases', 'duplicate_of']
+included_ids = set()
+with open('semscholar/titles_included.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        row = [r.lower() for r in row]
+        if row[1] == 'yes' or row[1] == 'maybe' or row[2] == 'yes' :
+            included_ids.add(int(row[0]))
+
+keys = ['id', 'databases', 'title', 'abstract', 'authors', 'year', 'publication_type', 'published_in', 'num_references', 'num_citations', 'url', 'doi', 'pdf']
 with open("semscholar/output.csv", 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(keys)
     for article in articles:
+        if article['id'] not in included_ids:
+            continue
         if 'databases' in article:
-            article['databases'] = ','.join(article['databases'])
+            article['databases'] = ', '.join(article['databases'])
         for key in keys:
             if key not in article:
                 article[key] = ''
