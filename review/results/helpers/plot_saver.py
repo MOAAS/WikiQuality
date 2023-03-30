a0d181521ab6ee6ed948d9f3526830390e94a791
MOAAS <miguel.moas@gmail.com>
2023-03-31 00:32:00 +0100

    Added dataset stats

--- review/results/helpers/plot_saver.py
+++ review/results/helpers/plot_saver.py
@@ -1,7 +1,9 @@
-def show_and_save(plt, file, size):
+def show_and_save(plt, file, size = None):
     ax = plt.gca()
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    plt.gcf().set_size_inches(size[0], size[1])
+
+    if size is not None:
+        plt.gcf().set_size_inches(size[0], size[1])
     plt.savefig(file, dpi=100, bbox_inches='tight', pad_inches=0.1)
     plt.show()
---
+++ review/results/stats.datasets.py
@@ -0,0 +1,104 @@
+from csvs.loader import inclusion_but_with_more as inclusion
+from csvs.loader import inclusion_but_as_dict as inclusion_dict
+from csvs.loader import general
+
+import helpers.plot_saver as plotsaver
+
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+for p in general:
+    p['Dataset Size'] = re.sub(r'\s+', '', p['Dataset Size'])
+papers_with_dataset = [p for p in general if p['Dataset Size'] != '?' and p['Dataset Size'] != 'N/A']
+
+papers_without_ml = [p for p in papers_with_dataset if p['ML'] == "N/A"]
+papers_with_ml = [p for p in papers_with_dataset if p['ML'] != "N/A"]
+papers_with_classical = [p for p in papers_with_dataset if 'CL' in p['ML']]
+papers_with_dl = [p for p in papers_with_dataset if 'DL' in p['ML']]
+
+def analyze_summary():
+    # average dataset size of ML and non-ML papers
+    print("========== SUMMARY ==========")
+
+    print("Average dataset size of ML papers: ", np.mean([int(p['Dataset Size']) for p in papers_with_ml]))
+    print("Average dataset size of non-ML papers: ", np.mean([int(p['Dataset Size']) for p in papers_without_ml]))
+    print("Average dataset size of all papers: ", np.mean([int(p['Dataset Size']) for p in papers_with_dataset]))
+    print("Average dataset size of CL papers: ", np.mean([int(p['Dataset Size']) for p in papers_with_classical]))
+    print("Average dataset size of DL papers: ", np.mean([int(p['Dataset Size']) for p in papers_with_dl]))
+
+def analyze_topics():
+    print("========== TOPICS ==========")
+    topics = {}
+    with_topic = [p for p in general if p['Topics'] != '?' and p['Topics'] != 'N/A']
+    for p in with_topic:
+        for t in p['Topics'].split(', '):
+            if t not in topics:
+                topics[t] = 0
+            topics[t] += 1
+    topics = {k: v for k, v in topics.items() if v > 1}
+    topics = sorted(topics.items(), key=lambda x: x[1], reverse=True)
+    for t in topics:
+        print(t[0], t[1])
+
+def analyze_languages():
+    print("========== LANGUAGES ==========")
+    langs = {}
+    with_langs = [p for p in general if p['Languages'] != '?' and p['Languages'] != 'N/A']
+    for p in with_langs:
+        for l in p['Languages'].split(', '):
+            if l not in langs:
+                langs[l] = 0
+            langs[l] += 1
+    langs = {k: v for k, v in langs.items() if v > 5}
+    langs = sorted(langs.items(), key=lambda x: x[1], reverse=True)
+
+    names = [l[0] for l in langs]
+    counts = [l[1] for l in langs]
+    plt.bar(names, counts)
+    plt.xticks(rotation=45)
+    plt.xlabel('Wikipedia Version')
+    plt.ylabel('Number of Publications')
+
+    # add labels to bars
+    for i, v in enumerate(counts):
+        plt.text(i, v + 0.5, str(v), ha='center')
+
+    plotsaver.show_and_save(plt, 'results/charts/languages.pdf', size=(8, 4))
+
+def analyze_overall_bubble():
+    info = [{
+        'ML': p['ML'],
+        'Year': int(inclusion_dict[p['Id']]['Year']),
+        'Size': int(p['Dataset Size']),
+        'Langs': len(p['Languages'].split(',')) if p['Languages'] != '?' else 1,        
+    } for p in papers_with_dataset]
+
+    # bubble chart: x is year, y is size, size is # langs, color is ML
+    plt.scatter(
+        [p['Year'] for p in info], 
+        [p['Size'] for p in info], 
+        s=[p['Langs'] * 20 for p in info], 
+        c=['#1f77b4' if p['ML'] == 'N/A' else '#ff7f0e' for p in info],
+        alpha=0.75
+    )
+    plt.xlabel('Publication Year')
+    plt.ylabel('Dataset Size (# Publications)')
+    plt.yscale('log')
+
+    # legend is only shown for the first plot
+    plt.scatter([], [], c='#1f77b4', label='Non-ML')
+    plt.scatter([], [], c='#ff7f0e', label='ML')
+    plt.legend()
+    
+    plt.xticks(np.arange(2005, 2025, 2))
+    plt.ylim(10, 10**8)
+
+    plotsaver.show_and_save(plt, 'results/charts/datasets.pdf')
+    
+
+analyze_overall_bubble()
+
+analyze_summary()
+analyze_topics()
+analyze_languages()
