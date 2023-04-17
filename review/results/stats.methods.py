f35490cb5a78c09b03628a1e321f13c554f2b47b
MOAAS <miguel.moas@gmail.com>
2023-04-17 16:15:48 +0100

    methods/languages double plot

--- review/results/stats.methods.py
+++ review/results/stats.methods.py
@@ -69,7 +69,6 @@ def languages_stats():
 
     plotsaver.show_and_save(plt, 'results/charts/languages.pdf', size=(8, 4))
 
-
     # count languages per paper
     counts = {}
     for p in with_langs:
@@ -83,24 +82,45 @@ def languages_stats():
             counts[num_langs]['Non-ML'] += 1
         else:
             counts[num_langs]['ML'] += 1
-
     # sort by count
     counts = sorted(counts.items(), key=lambda x: x[0])
+    counts_overone = [(c[0], c[1]) for c in counts if c[0] > 1]
 
-    # stacked bar chart
-    names = [c[0] for c in counts]
-    ml_counts = [c[1]['ML'] for c in counts]
-    non_ml_counts = [c[1]['Non-ML'] for c in counts]
+    print('Number of papers with one language: (non-ml/ml)', str(counts[0][1]['Non-ML']), counts[0][1]['ML'])
 
-    plt.bar(names, non_ml_counts, label='Non-ML')
-    plt.bar(names, ml_counts, bottom=non_ml_counts, label='ML')
-    plt.xlabel('Number of asssessed Wikipedia Versions')
-    plt.ylabel('Number of Publications')
-    plt.legend()
+    def draw_plot(plt, counts):
+        # stacked bar chart
+        names = [c[0] for c in counts]
+        ml_counts = [c[1]['ML'] for c in counts]
+        non_ml_counts = [c[1]['Non-ML'] for c in counts]
+
+        plt.bar(names, non_ml_counts, label='Non-ML')
+        plt.bar(names, ml_counts, bottom=non_ml_counts, label='ML')
+
+    fig, main_axis = plt.subplots(figsize=(8, 4))
+    draw_plot(main_axis, counts)
+    main_axis.set_xlabel('Number of asssessed Wikipedia Versions')
+    main_axis.set_ylabel('Number of Publications')
+
+    # add grid to main plot, and move grid to back
+    main_axis.grid(True, axis='both', which='both', color='0.8', linestyle=':', linewidth=1, zorder=1)
+    main_axis.set_axisbelow(True)
+    for spine in main_axis.spines.values():
+        spine.set_zorder(2)
     
+    small_axis = fig.add_axes([0.3, 0.4, 0.58, 0.45])
+    draw_plot(small_axis, counts_overone)
+    small_axis.set_xticks([c[0] for c in counts_overone])
+    small_axis.set_xlim(1, 60)
+    small_axis.tick_params(axis='both', which='major', labelsize=8)
+    small_axis.set_yticks([0, 1, 2, 3])
+    small_axis.legend()
+
+    # add arrow between plots
+    main_axis.annotate('', xytext=(6, 22), xy=(11, 42),  arrowprops=dict(facecolor='black', arrowstyle='<->'))
+
     plotsaver.show_and_save(plt, 'results/charts/languages_count.pdf', size=(8, 4))
     print('Number of papers per num languages: ', counts)
-
 
 method_stats()
 languages_stats()
